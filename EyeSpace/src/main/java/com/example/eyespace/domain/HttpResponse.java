3fdb77f5d2559642e18d3f4fd93a137cc6ae8e67
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-04-15 17:38:04 +0300

    User Principal

---
+++ EyeSpace/src/main/java/com/example/eyespace/domain/HttpResponse.java
@@ -0,0 +1,52 @@
+package com.example.eyespace.domain;
+
+import org.springframework.http.HttpStatus;
+
+public class HttpResponse {
+
+    private int httpStatusCode; // 200,201,400,500
+    private HttpStatus httpStatus;
+    private String reason;
+    protected String message;
+
+    public HttpResponse() {}
+
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
+
+    public int getHttpStatusCode() {
+        return httpStatusCode;
+    }
+
+    public void setHttpStatusCode(int httpStatusCode) {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
