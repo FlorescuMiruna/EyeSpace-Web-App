96fd915bb5d06304281918a9ed62b4d5764cd6ca
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-04-15 17:14:40 +0300

    User domain

---
+++ EyeSpace/src/main/java/com/example/eyespace/EyeSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.eyespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EyeSpaceApplication.class, args);
+    }
+
+}
---
+++ EyeSpace/src/test/java/com/example/eyespace/EyeSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eyespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyeSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
