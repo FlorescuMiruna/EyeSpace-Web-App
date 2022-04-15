e10bfb64531d6fa5f64ced0f563ebe0a5f482b54
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-04-15 18:31:50 +0300

    Security constants

---
+++ EyeSpace/src/main/java/com/example/eyespace/resource/UserResource.java
@@ -0,0 +1,15 @@
+package com.example.eyespace.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping("/home")
+    public String showUser(){
+        return "App works";
+    }
+}
