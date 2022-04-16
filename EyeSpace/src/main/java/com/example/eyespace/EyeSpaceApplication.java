aa82463281faea3ed0c734a5c98424e874474baa
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-04-16 17:54:33 +0300

    Added security configuration

--- EyeSpace/src/main/java/com/example/eyespace/EyeSpaceApplication.java
+++ EyeSpace/src/main/java/com/example/eyespace/EyeSpaceApplication.java
@@ -2,12 +2,19 @@ package com.example.eyespace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class EyeSpaceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EyeSpaceApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
