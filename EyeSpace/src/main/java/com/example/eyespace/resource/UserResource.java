34166d18167da946152d9e9ae6c4013da73a56dd
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-04-18 18:09:20 +0300

    Login and generate JWT

--- EyeSpace/src/main/java/com/example/eyespace/resource/UserResource.java
+++ EyeSpace/src/main/java/com/example/eyespace/resource/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.eyespace.resource;
 
 import com.example.eyespace.domain.User;
+import com.example.eyespace.domain.UserPrincipal;
 import com.example.eyespace.exception.ExceptionHandling;
 import com.example.eyespace.exception.domain.EmailExistException;
 import com.example.eyespace.exception.domain.UserNotFoundException;
@@ -8,11 +9,13 @@ import com.example.eyespace.exception.domain.UsernameExistException;
 import com.example.eyespace.service.UserService;
 import com.example.eyespace.utility.JWTTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
+import static com.example.eyespace.constant.SecurityConstant.JWT_TOKEN_HEADER;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -29,7 +32,14 @@ public class UserResource extends ExceptionHandling {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        authenticate(user.getUsername(), user.getPassword());
+        User loginUser = userService.findUserByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(loginUser, jwtHeader, OK);
+    }
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody User user) throws UserNotFoundException, UsernameExistException, EmailExistException {
@@ -37,6 +47,13 @@ public class UserResource extends ExceptionHandling {
         return new ResponseEntity<>(newUser, OK);
     }
 
+    private HttpHeaders getJwtHeader(UserPrincipal user) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(user));
+        return headers;
+    }
 
-
+    private void authenticate(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    }
 }
