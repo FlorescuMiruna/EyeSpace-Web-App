d9b9abef3b577f98fc9d332c686249af751dfcd3
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-04-18 20:20:48 +0300

    Added email constants

---
+++ EyeSpace/src/main/java/com/example/eyespace/service/EmailService.java
@@ -0,0 +1,29 @@
+package com.example.eyespace.service;
+
+import com.sun.mail.smtp.SMTPTransport;
+import org.springframework.stereotype.Service;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+import static com.example.eyespace.constant.EmailConstant.*;
+
+@Service
+public class EmailService {
+
+
+
+    private Session getEmailSession() {
+        Properties properties = System.getProperties();
+        properties.put(SMTP_HOST, GMAIL_SMTP_SERVER);
+        properties.put(SMTP_AUTH, true);
+        properties.put(SMTP_PORT, DEFAULT_PORT);
+        properties.put(SMTP_STARTTLS_ENABLE, true);
+        properties.put(SMTP_STARTTLS_REQUIRED, true);
+        return Session.getInstance(properties, null);
+    }
+}
