99bf46fd44eb603800bf7a88c6c27867a62b7da4
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-04-23 21:24:21 +0300

    Notification module

--- eyespaceapp/src/app/app.module.ts
+++ eyespaceapp/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AuthenticationService } from './service/authentication.service';
 import { UserService } from './service/user.service';
 import { AuthInterceptor } from './interceptor/auth.interceptor';
 import { AuthenticationGuard } from './guard/authentication.guard';
+import { NotificationModule } from './notification.module';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { AuthenticationGuard } from './guard/authentication.guard';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NotificationModule
+    
   ],
   providers: [ AuthenticationGuard, AuthenticationService, UserService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true } ],
