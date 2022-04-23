34fb9c2111902a00049ef173e84850554930db67
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-04-23 21:41:47 +0300

    Notification Service with Enum notification types

--- eyespaceapp/src/app/app.module.ts
+++ eyespaceapp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { UserService } from './service/user.service';
 import { AuthInterceptor } from './interceptor/auth.interceptor';
 import { AuthenticationGuard } from './guard/authentication.guard';
 import { NotificationModule } from './notification.module';
+import { NotificationService } from './service/notification.service';
 
 
 @NgModule({
@@ -22,7 +23,7 @@ import { NotificationModule } from './notification.module';
     NotificationModule
     
   ],
-  providers: [ AuthenticationGuard, AuthenticationService, UserService,
+  providers: [ NotificationService,AuthenticationGuard, AuthenticationService, UserService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true } ],
   bootstrap: [AppComponent]
 })
--- eyespaceapp/src/app/guard/authentication.guard.ts
+++ eyespaceapp/src/app/guard/authentication.guard.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { NotificationType } from '../enum/notification-type.enum';
 import { AuthenticationService } from '../service/authentication.service';
+import { NotificationService } from '../service/notification.service';
 
 @Injectable({providedIn: 'root'})
 export class AuthenticationGuard implements CanActivate {
 
-  constructor(private authenticationService: AuthenticationService, private router: Router) {}
+  constructor(private authenticationService: AuthenticationService, private router: Router, private notificationService:NotificationService) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     return this.isUserLoggedIn();
@@ -17,7 +19,7 @@ export class AuthenticationGuard implements CanActivate {
       return true;
     }
     this.router.navigate(['/login']);
-//TODO: Send notification to user
+    this.notificationService.notify(NotificationType.ERROR, `You need to log in to access this page`);
     return false;
   }
 
