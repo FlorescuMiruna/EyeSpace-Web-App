2513a342800bfb7452b1f1f23af2100ff3ec8647
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-04-26 20:26:07 +0300

    Page header, tab name

--- eyespaceapp/src/app/user/user.component.ts
+++ eyespaceapp/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  private titleSubject = new BehaviorSubject<String>('Users');
+  public titleAction$ = this.titleSubject.asObservable();
 
   constructor() { }
+
+  
+  public changeTitle(title: string): void {
+    this.titleSubject.next(title);
+  }
 
   ngOnInit(): void {
   }
