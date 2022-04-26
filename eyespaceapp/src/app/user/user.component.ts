bc612651ccf42aaab81cf132bce119ea38652d89
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-04-26 20:44:05 +0300

    getUsers

--- eyespaceapp/src/app/user/user.component.ts
+++ eyespaceapp/src/app/user/user.component.ts
@@ -1,5 +1,12 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { NotificationType } from '../enum/notification-type.enum';
+import { User } from '../model/user';
+import { AuthenticationService } from '../service/authentication.service';
+import { NotificationService } from '../service/notification.service';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-user',
@@ -9,15 +16,47 @@ import { BehaviorSubject } from 'rxjs';
 export class UserComponent implements OnInit {
   private titleSubject = new BehaviorSubject<String>('Users');
   public titleAction$ = this.titleSubject.asObservable();
+  public users: User[] = [];
+  public refreshing: any;
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private router: Router, private authenticationService: AuthenticationService,
+    private userService: UserService, private notificationService: NotificationService) {}
 
   
   public changeTitle(title: string): void {
     this.titleSubject.next(title);
   }
+  public getUsers(showNotification: boolean): void {
+    this.refreshing = true;
+    this.subscriptions.push(
+      this.userService.getUsers().subscribe(
+        (response: User[]) => {
+          this.userService.addUsersToLocalCache(response);
+          this.users = response;
+          this.refreshing = false;
+          if (showNotification) {
+            this.sendNotification(NotificationType.SUCCESS, `${response.length} user(s) loaded successfully.`);
+          }
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.refreshing = false;
+        }
+      )
+    );
+
+  }
+  private sendNotification(notificationType: NotificationType, message: string): void {
+    if (message) {
+      this.notificationService.notify(notificationType, message);
+    } else {
+      this.notificationService.notify(notificationType, 'An error occurred. Please try again.');
+    }
+  }
 
   ngOnInit(): void {
+    this.getUsers(true);
   }
 
 }
