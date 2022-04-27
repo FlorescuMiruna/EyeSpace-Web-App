c659993ab300807ede15efb2559a1ceb9c5d6944
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-04-27 21:49:43 +0300

    Profile, edit profile and change profile picture

--- eyespaceapp/src/app/user/user.component.ts
+++ eyespaceapp/src/app/user/user.component.ts
@@ -1,10 +1,12 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { HttpErrorResponse, HttpEvent, HttpEventType } from '@angular/common/http';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { NotificationType } from '../enum/notification-type.enum';
+import { Role } from '../enum/role.enum';
 import { CustomHttpRespone } from '../model/custom-http-response';
+import { FileUploadStatus } from '../model/file-upload.status';
 import { User } from '../model/user';
 import { AuthenticationService } from '../service/authentication.service';
 import { NotificationService } from '../service/notification.service';
@@ -15,7 +17,7 @@ import { UserService } from '../service/user.service';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   private titleSubject = new BehaviorSubject<String>('Users');
   public titleAction$ = this.titleSubject.asObservable();
   public users: User[] = [];
@@ -28,6 +30,7 @@ export class UserComponent implements OnInit {
   public profileImage: any;
   public editUser = new User();
   private currentUsername: string = "";
+  public fileStatus = new FileUploadStatus();
 
   constructor(private router: Router, private authenticationService: AuthenticationService,
     private userService: UserService, private notificationService: NotificationService) { }
@@ -35,6 +38,9 @@ export class UserComponent implements OnInit {
   ngOnInit(): void {
     this.user = this.authenticationService.getUserFromLocalCache();
     this.getUsers(true);
+  }
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
   public changeTitle(title: string): void {
@@ -67,6 +73,23 @@ export class UserComponent implements OnInit {
       this.notificationService.notify(notificationType, 'An error occurred. Please try again.');
     }
   }
+  public onLogOut(): void {
+    this.authenticationService.logOut();
+    this.router.navigate(['/login']);
+    this.sendNotification(NotificationType.SUCCESS, `You've been successfully logged out`);
+  }
+
+  public get isAdmin(): boolean {
+    return true;
+  }
+
+  public get isManager(): boolean {
+    return true;
+  }
+
+  public get isAdminOrManager(): boolean {
+    return true;
+  }
 
 
   // public f(): void {
@@ -77,13 +100,15 @@ export class UserComponent implements OnInit {
     this.clickButton('openUserInfo');
   }
   private clickButton(buttonId: string): void {
-
+    console.log("click")
     const element = document.getElementById(buttonId);
     if (element !== null)
       element.click();
   }
 
   public onProfileImageChange(fileName: string, profileImage: File): void {
+    console.log("** onProfileImageChange")
+    console.log(fileName,profileImage);
     this.fileName =  fileName;
     this.profileImage = profileImage;
     console.log("Name:",fileName);
@@ -185,6 +210,71 @@ export class UserComponent implements OnInit {
       )
     );
   } 
+  public onUpdateProfileImage(): void {
+    console.log("** onUpdateProfileImage")
+    const formData = new FormData();
+    formData.append('username', this.user.username);
+    formData.append('profileImage', this.profileImage);
+    this.subscriptions.push(
+      this.userService.updateProfileImage(formData).subscribe(
+        (event: HttpEvent<any>) => {
+          this.reportUploadProgress(event);
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.fileStatus.status = 'done';
+        }
+      )
+    );
+  }
+  public onUpdateCurrentUser(user: User): void {
+    this.refreshing = true;
+    this.currentUsername = this.authenticationService.getUserFromLocalCache().username;
+    const formData = this.userService.createUserFormDate(this.currentUsername, user, this.profileImage);
+    this.subscriptions.push(
+      this.userService.updateUser(formData).subscribe(
+        (response: User) => {
+          this.authenticationService.addUserToLocalCache(response);
+          this.getUsers(false);
+          this.fileName = null;
+          this.profileImage = null;
+          this.sendNotification(NotificationType.SUCCESS, `${response.firstName} ${response.lastName} updated successfully`);
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.refreshing = false;
+          this.profileImage = null;
+        }
+      )
+      );
+  }
+  private reportUploadProgress(event: HttpEvent<any>): void {
+    switch (event.type) {
+      case HttpEventType.UploadProgress:
+        if(event.total)
+        this.fileStatus.percentage = Math.round(100 * event.loaded / event.total);
+        this.fileStatus.status = 'progress';
+        break;
+      case HttpEventType.Response:
+        if (event.status === 200) {
+          this.user.profileImageUrl = `${event.body.profileImageUrl}?time=${new Date().getTime()}`;
+          this.sendNotification(NotificationType.SUCCESS, `${event.body.firstName}\'s profile image updated successfully`);
+          this.fileStatus.status = 'done';
+          break;
+        } else {
+          this.sendNotification(NotificationType.ERROR, `Unable to upload image. Please try again`);
+          break;
+        }
+      default:
+        `Finished all processes`;
+    }
+  }
+
+  public updateProfileImage(): void {
+    console.log("before click??")
+    this.clickButton('profile-image-input');
+  }
+
 
 }
 
