e6903cb8c0df02149f7ab7640262d798b9d9aff7
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-06-01 18:20:05 +0300

    Quote add, edit, delete + style

--- eyespaceapp/src/app/component/quote/quote.component.ts
+++ eyespaceapp/src/app/component/quote/quote.component.ts
@@ -1,11 +1,17 @@
 
 import { Component, OnInit } from '@angular/core';
+import { FormGroup ,FormBuilder} from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { Movie } from 'src/app/model/movie';
 import { Quote } from 'src/app/model/quote';
 
 
 import { User } from 'src/app/model/user';
 import { AuthenticationService } from 'src/app/service/authentication.service';
 import { QuoteService } from 'src/app/service/quote.service';
+import { UserService } from 'src/app/service/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-quote',
@@ -16,25 +22,62 @@ export class QuoteComponent implements OnInit {
 
    user: User = this.authenticationService.getUserFromLocalCache();
    quotes: Quote[] = [];
+   movies: Movie[] = [];
+   myQuoteObj: Quote = new Quote();
+   myQuoteObjEdit : Quote = new Quote();
+   testVar : Boolean = true;
 
-  constructor(private authenticationService: AuthenticationService,private quoteService: QuoteService) { }
+   quoteDetails !: FormGroup;
+  quoteDetailsEdit !: FormGroup;
+  
+
+  constructor(private authenticationService: AuthenticationService,private quoteService: QuoteService,private router: Router,private formBuilder: FormBuilder, private userService: UserService) { }
 
   ngOnInit(): void {
 
+
+    this.quoteDetails = this.formBuilder.group({
+
+      text: [''],
+      // person:[''],
+      theme:[''],
+      movieId: ['']
+ 
+    });
+
+    this.quoteDetailsEdit = this.formBuilder.group({
+      id: [''],
+      textt: [''],
+      // personn: [''],
+      themee:['']
+      
+    });
+
+
     this.initializeQuotes();
+    this.initializeMovies();
+
   }
 
+
+
   initializeQuotes() {
+
+
     this.quoteService.getAllQuotesByUserId(this.user.id).subscribe(res => {
 
 
       this.quotes = res;
       console.log(res);
-      console.log(this.quotes[0].movie.posterUrl)
-
-      // this.quotes[] = res[0];
-
-      /**Iau din lista doar comentariile carora le-am dat like*/
+   
+      
+    this.quoteDetails = this.formBuilder.group({
+      id: [''],
+      text: [''],
+      // person:[''],
+      theme:[''],
+      movieId: ['']
+    });
 
 
     }, err => {
@@ -43,5 +86,202 @@ export class QuoteComponent implements OnInit {
     });
 
   }
+
+  addQuote(){
+
+    this.myQuoteObj.text = this.quoteDetails.value.text;
+    // this.myQuoteObj.person = this.quoteDetails.value.person;
+    this.myQuoteObj.theme = this.quoteDetails.value.theme;
+
+    var movieId = this.quoteDetails.value.movieId;
+    console.log("Form",this.quoteDetails.value)
+    console.log("OBJ",this.myQuoteObj)
+  
+
+    let userId = this.authenticationService.getUserFromLocalCache().id;
+
+    this.quoteService.addQuote(this.myQuoteObj, movieId, userId).subscribe(res => {
+      // console.log("res", res);
+      this.initializeQuotes();
+      Swal.fire({
+        position: 'center',
+        // imageUrl: res.movie.posterUrl,
+        // imageHeight: 150,
+        // imageWidth: 150,
+        icon: 'success',
+        title: `The quote from ${res.movie.title} was added to your list`,
+        showConfirmButton: false,
+        timer: 2000
+      })
+
+    }, err => {
+      console.log("EROARE");
+      console.log(err);
+
+    });
+
+  }
+
+  editQuote(quote:Quote){
+
+    this.quoteDetailsEdit.controls['id'].setValue(quote.id);
+    this.quoteDetailsEdit.controls['textt'].setValue(quote.text);
+    // this.quoteDetailsEdit.controls['personn'].setValue(quote.person);
+    this.quoteDetailsEdit.controls['themee'].setValue(quote.theme);
+   
+
+    console.log(this.quoteDetailsEdit.value)
+ 
+
+  }
+
+  updateQuote(){
+
+    this.myQuoteObjEdit.id = this.quoteDetailsEdit.value.id;
+    this.myQuoteObjEdit.text = this.quoteDetailsEdit.value.textt;
+    // this.myQuoteObjEdit.person = this.quoteDetailsEdit.value.personn;
+    this.myQuoteObjEdit.theme = this.quoteDetailsEdit.value.themee;
+
+    console.log("OBJ", this.myQuoteObjEdit);
+    
+    this.quoteService.updateQuote(this.myQuoteObjEdit).subscribe(res=>{
+      console.log("res",res);
+
+      this.quoteDetailsEdit = this.formBuilder.group({
+        id : [''],
+        textt : [''],
+        // personn: [''],
+        themee:['']
+
+  
+      });  
+     
+      this.initializeQuotes();
+    },err=>{
+      console.log(err);
+    })
+
+  }
+  deleteQuote(quote: Quote) {
+    Swal.fire({
+  title: 'Are you sure you want to delete your quote?',
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#4E9A9B',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  if (result.isConfirmed) {
+
+
+    this.quoteService.deleteQuote(quote).subscribe(res => {
+      Swal.fire(
+        'The comment was deleted!',
+      )
+      this.initializeQuotes();
+    }, err => {
+      
+      console.log("ERROR:", err);
+
+    })
+
+
+
+
+
+
+  }
+})
+
+
+}
+
+  refreshUserFromLocalChash(id: number) {
+
+    this.userService.getUserById(id).subscribe(res => {
+
+      this.authenticationService.addUserToLocalCache(res);
+
+
+    }, err => {
+      console.log("Error while fetching data.")
+    });
+  }
+
+  initializeMovies(){
+    this.refreshUserFromLocalChash(this.authenticationService.getUserFromLocalCache().id);
+    console.log("USER",this.authenticationService.getUserFromLocalCache())
+
+    this.movies = this.authenticationService.getUserFromLocalCache().movies;
+
+    console.log("MOVIES:", this.movies)
+
+  
+   
+
+  }
+  
+
+  calculateClass(){
+    return "additional";
+
+  }
+  goToProfile(){
+    this.router.navigate(['/user/management']);
+    localStorage.setItem('page', 'Profile');
+  }
+
+  goToUsers(){
+    this.router.navigate(['/user/management']);
+    localStorage.setItem('page', 'Users');
+  }
+
+  goToSettings(){
+    this.router.navigate(['/user/management']);
+    localStorage.setItem('page', 'Settings');
+  }
+  goToMyMovies(){
+    this.router.navigate(['/my-movies']);
+    localStorage.setItem('my-movies', 'All');
+
+  }
+  
+  goToMyWatchedMovies(){
+    this.router.navigate(['/my-movies']);
+    localStorage.setItem('my-movies', 'Watched');
+  }
+  goToMyWatchListMovies(){
+    this.router.navigate(['/my-movies']);
+    localStorage.setItem('my-movies', 'WatchList');
+  }
+
+  goToMyFavoriteMovies(){
+    this.router.navigate(['/my-movies']);
+    localStorage.setItem('my-movies', 'Favorites');
+
+  }
+  public  isDark(theme: String): boolean {
+    if (theme == "Dark")
+      return true;
+    else return false;
+  }
+  public  isLight(theme: String): boolean {
+    if (theme == "Light")
+      return true;
+    else return false;
+  }
+  public  isBlue(theme: String): boolean {
+    if (theme == "Blue")
+      return true;
+    else return false;
+  }
+
+  public  isPink(theme: String): boolean {
+    if (theme == "Pink")
+      return true;
+    else return false;
+  }
+
+
 
 }
--- eyespaceapp/src/app/service/quote.service.ts
+++ eyespaceapp/src/app/service/quote.service.ts
@@ -11,14 +11,36 @@ import { Quote } from '../model/quote';
 export class QuoteService {
     
   private host = environment.apiUrl;
+
   getAllQuotesByUserIdURL : string = '';
+  addQuoteURL: string;
+  deleteQuoteURL: string;
+  updateQuoteURL: string;
+
   constructor(private http :HttpClient) { 
     this.getAllQuotesByUserIdURL = `${this.host}/quote/user/`;
+    this.addQuoteURL = `${this.host}/quote/movie/`;
+    this.deleteQuoteURL = `${this.host}/quote/`;
+    this.updateQuoteURL = `${this.host}/quote/`;
   }
 
   getAllQuotesByUserId(userId: number): Observable<Quote[]> {
 
     var URL = this.getAllQuotesByUserIdURL + userId;
     return this.http.get<Quote[]>(URL);
+  }
+
+  addQuote(quote : Quote, movieId : string, userId: number ): Observable<Quote> {
+    var URL = this.addQuoteURL + movieId + '/user/' + userId;
+    console.log(URL);
+    return this.http.post<Quote>(URL,quote);
+  }
+  deleteQuote(quote: Quote): Observable<Quote> {
+    return this.http.delete<Quote>(this.deleteQuoteURL +quote.id);
+  }
+
+  updateQuote(quote : Quote): Observable<Quote>{
+    return this.http.put<Quote>(this.updateQuoteURL + quote.id, quote);
+
   }
 }
